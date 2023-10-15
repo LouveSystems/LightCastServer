64cb4831ffafc72a54c1fc02251e85c08eac79f4
Louve <louve@systems>
2023-10-15 12:07:55 +0100

    Do not ask client to punch

--- controllers/ask_them_to_punch_me.js
+++ controllers/ask_them_to_punch_me.js
@@ -12,14 +12,14 @@ module.exports = function(data, recvinfo, server)
     // for later use
     const lightcastVersion = buff.readInt8();
     
-    const intIP = buff.readUInt32();
-    const address = int2ip(intIP);
-    const port = buff.readUInt16();
+    const serverAddressInt = buff.readUInt32();
+    const serverAddress = int2ip(serverAddressInt);
+    const serverPort = buff.readUInt16();
     const gameName = buff.readFixedLengthString(GAME_NAME_LENGTH)
     
-    logger.debug(`Client [${recvinfo.address}] asked to be punched by lobby ${address}:${port}`);
+    logger.debug(`Client [${recvinfo.address}] asked to be punched by lobby ${serverAddress}:${serverPort}`);
 
-    const lobby = server.getLobby(intIP, port);
+    const lobby = server.getLobby(serverAddressInt, serverPort);
     if (lobby)
     {
         if (lobby.getGameName() == gameName)
@@ -29,16 +29,16 @@ module.exports = function(data, recvinfo, server)
             header.writeUInt32LE(ip2int(recvinfo.address), 1);
             header.writeUInt16LE(recvinfo.port, 4 + 1);
             
-            server.sendTo(Buffer.concat([ header, lobby.serialize()]), recvinfo);
-            logger.info(`Asked ${address}:${port} on ${gameName} to punch ${recvinfo.address}`);
+            server.sendTo(Buffer.concat([ header, lobby.serialize()]), {address: serverAddress, port: serverPort });
+            logger.info(`Asked ${serverAddress}:${serverPort} on ${gameName} to punch ${recvinfo.address}`);
         }
         else
         {
-            logger.warn(`Could not find the lobby (${address}:${port} - wrong game name, expected ${lobby.getGameName()} and got ${gameName}) that ${recvinfo.address} is talking about.`)
+            logger.warn(`Could not find the lobby (${serverAddress}:${serverPort} - wrong game name, expected ${lobby.getGameName()} and got ${gameName}) that ${recvinfo.address} is talking about.`)
         }
     }
     else
     {
-        logger.warn(`Could not find the lobby (${address}:${port} that ${recvinfo.address} is talking about.`)
+        logger.warn(`Could not find the lobby (${serverAddress}:${serverPort} that ${recvinfo.address} is talking about.`)
     }
 }
